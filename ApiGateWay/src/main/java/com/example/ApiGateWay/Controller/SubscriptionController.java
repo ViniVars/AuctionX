def185df834b6e568f06a05199cea7074ff32d29
ViniVars <dasarinirmala110@gmail.com>
2025-10-02 09:48:26 +0530

    Added FilterParams DTO for Filter Structuring

--- ApiGateWay/src/main/java/com/example/ApiGateWay/Controller/SubscriptionController.java
+++ ApiGateWay/src/main/java/com/example/ApiGateWay/Controller/SubscriptionController.java
@@ -3,6 +3,7 @@ package com.example.ApiGateWay.Controller;
 
 import com.example.ApiGateWay.Dto.SubscriptionResponse;
 import com.example.ApiGateWay.Dto.UserRequest;
+import com.example.ApiGateWay.Dto.filterParams;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,9 +18,9 @@ public class SubscriptionController {
     ResponseEntity<List<SubscriptionResponse>> getCBidSubs(
             @RequestParam String bidId,
             @RequestParam String userId){
-        List<String> filters = new ArrayList<>();
-        if(bidId != null) filters.add(bidId);
-        if(userId != null) filters.add(userId);
+        List<filterParams> filters = new ArrayList<>();
+        if(bidId != null) filters.add(new filterParams("bidId", bidId));
+        if(userId != null) filters.add(new filterParams("userId", userId));
         return getSBidSubs(filters);
     }
 
---
+++ ApiGateWay/src/main/java/com/example/ApiGateWay/Dto/filterParams.java
@@ -0,0 +1,14 @@
+package com.example.ApiGateWay.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class filterParams {
+
+    private String key;
+    private String value;
+}
