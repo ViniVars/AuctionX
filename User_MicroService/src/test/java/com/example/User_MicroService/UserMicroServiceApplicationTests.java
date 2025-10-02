c1cd5fc5485a6d0052575362875f9954ecbecb08
ViniVars <dasarinirmala110@gmail.com>
2025-10-02 10:53:31 +0530

    Made User MS Initial SetUp

---
+++ User_MicroService/src/test/java/com/example/User_MicroService/UserMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.User_MicroService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
